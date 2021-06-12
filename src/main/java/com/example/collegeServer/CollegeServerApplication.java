c1758b08ba216c787427801b556822671af3d966
BarakovaES <barakova.2000@mail.ru>
2021-06-12 22:18:48 +0400

    Initial commit

---
+++ src/main/java/com/example/collegeServer/CollegeServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.collegeServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/collegeServer/CollegeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.collegeServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
